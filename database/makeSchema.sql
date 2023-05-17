cfdb16b2c553cc26d3674fee6b4e8a066a8f630a
danielxhogan <danielxhogan@gmail.com>
2023-05-16 17:42:22 -0700

    initial database docker config

---
+++ database/makeSchema.sql
@@ -0,0 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+  id uuid DEFAULT uuid_generate_v4(),
+  username VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL
+);
