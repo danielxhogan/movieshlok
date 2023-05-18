86014c335f48066a670e1014b0324760b1bcff17
danielxhogan <danielxhogan@gmail.com>
2023-05-17 21:13:06 -0700

    delete unnecessary

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,53 +1,5 @@
-export type Post = {
-  slug: String;
-  title: String;
-  content: String;
-  postId: React.Key;
-}
-
-type Posts = {
-  nodes: Post[]
-}
-
-type Props = {
-  posts: Posts;
-}
-
-export default function HomePage({ posts }: Props) {
-  console.log(posts);
+export default function HomePage() {
   return <>
-     {/* { posts.nodes.map(post => {
-       return (
-         <ul key={post.postId}>{post.title}</ul>
-       )
-     })}
-    <h1>Wa Suh Dude!!!</h1> */}
+    <h1>If you don&apos;t like movies then you can get out</h1>
   </>
 }
-
-// export async function getStaticProps() {
-//   const res = await fetch("http://localhost/graphql", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify({
-//       query: `
-//         query NewQuery {
-//           posts {
-//             nodes {
-//               slug
-//               title
-//               postId
-//             }
-//           }
-//         }
-//       `,
-//     })
-//   })
-//   const response = await res.json();
-
-//   return {
-//     props: {
-//       posts: response.data.posts,
-//     },
-//   }
-// }
