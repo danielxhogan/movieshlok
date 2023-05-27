f0ddf0ed3f68fdbb54a68dcccb56800febedd101
danielxhogan <danielxhogan@gmail.com>
2023-05-26 23:06:26 -0700

    remove .sh files

--- backend/dockerrun.sh
+++
@@ -1,6 +0,0 @@
-docker run \
---name movieshlok-backend \
---network=host \
--e DATABASE_URL=postgres://postgres:movieshlok@localhost:5432/movieshlok \
--dp 3030:3030 \
-danielxhogan/movieshlok-backend
