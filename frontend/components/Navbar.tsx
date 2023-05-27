df2198e3d13aff3ff30b9fb26765c4d7ced60117
danielxhogan <danielxhogan@gmail.com>
2023-05-27 09:34:26 -0700

    handle logout

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -26,20 +26,39 @@ export default function Navbar() {
   const [ authenticated, setAuthenticated ] = useState(false);
   const [ username, setUsername ] = useState<String | null>(null);
 
+  // set current location in localStorage unless on login or register page
+  // used to send user back to the page they were on after successful login
   useEffect(() => {
     const currentLocation = window.location.href;
 
     if (currentLocation.indexOf("login") === -1 && currentLocation.indexOf("register") === -1) {
       localStorage.setItem("currentLocation", currentLocation);
     }
-  }, [])
+  }, []);
+
+  // look for auth cookies and set authentication status for the page
+  useEffect(() => {
+    const cookiesString = document.cookie;
+    const cookies = cookiesString.split(";");
+
+    cookies.forEach(cookieString => {
+      const cookie = cookieString.split("=");
+
+      if (cookie[0].trim() === "jwt_token") {
+        setAuthenticated(true);
+      }
+
+      if (cookie[0].trim() === "username") {
+        setUsername(cookie[1]);
+      }
+    });
+  }, []);
 
   function onClickLogOut() {
-    localStorage.removeItem("username");
-    localStorage.removeItem("authToken");
-    localStorage.removeItem("refreshToken");
-
-    window.location.reload();
+    document.cookie = "username=";
+    document.cookie = "jwt_token=";
+    setAuthenticated(false);
+    setUsername(null);
   }
 
   return <>
@@ -59,7 +78,7 @@ export default function Navbar() {
 
           {/* authenticated & large screen */}
           { authenticated && username ? <>
-              <Link href={`/u/${username}/profie`}> Profile </Link>
+              <Link href={`/u/${username}/profile`}> Profile </Link>
               <Link href={`/u/${username}/movies`}> Movies </Link>
               <Link href={`/u/${username}/lists`}> Lists </Link>
               <Link href={`/u/${username}/calendar`}> Calendar </Link>
@@ -81,7 +100,7 @@ export default function Navbar() {
         </div>
 
         { authenticated && username ?
-        <div clssName={styles["auth-menu"]}>
+        <div className={styles["auth-menu"]}>
 
           {/* authenticated */}
           <Link href="/auth/notifications">
@@ -100,7 +119,7 @@ export default function Navbar() {
               {/* authenticated and small screen */}
               <div className={styles["small-auth-menu"]}>
 
-                <Link href={`/u/${username}/profie`}>
+                <Link href={`/u/${username}/profile`}>
                   <MenuItem command="Profile"></MenuItem>
                 </Link>
 
