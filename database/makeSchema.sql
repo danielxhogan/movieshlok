343d5dce7f2236a23ff2e6dfccea18072a7bfc6b
danielxhogan <danielxhogan@gmail.com>
2023-06-03 15:09:00 -0700

    add reviews table

--- database/makeSchema.sql
+++ database/makeSchema.sql
@@ -8,3 +8,14 @@ CREATE TABLE users (
   first_name VARCHAR(255),
   last_name VARCHAR(255)
 );
+
+CREATE TYPE star AS ENUM ('0', '1', '2', '3', '4', '5');
+
+CREATE TABLE reviews (
+  id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_id uuid NOT NULL references users(id),
+  movie_id VARCHAR(255) NOT NULL,
+  rating star,
+  review TEXT NOT NULL,
+  liked BOOLEAN
+);
