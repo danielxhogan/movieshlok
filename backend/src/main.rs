2273d73287b5533c2a64843aa5c68ddb3b8f8f5b
danielxhogan <danielxhogan@gmail.com>
2023-06-15 19:23:35 -0700

    server hosts on port 0000

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -31,5 +31,5 @@ async fn main() {
     .with(cors);
 
   println!("Starting server on port 3030...");
-  warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+  warp::serve(routes).run(([0, 0, 0, 0], 3030)).await;
 }
