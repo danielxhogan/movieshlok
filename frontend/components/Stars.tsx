b08efd7e0ef307be5b512a7e80403517e2cae3cf
danielxhogan <danielxhogan@gmail.com>
2023-07-13 18:02:53 -0700

    change getElementbyId to useRef

--- frontend/components/Stars.tsx
+++ frontend/components/Stars.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/components/Stars.module.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 export enum Rating {
   ZERO,
@@ -27,16 +27,16 @@ export default function Stars(props: Props) {
   const [currentRating, setCurrentRating] = useState(props.initialRating);
   const [newRating, setNewRating] = useState(currentRating);
 
+  const starOneRef = useRef<HTMLElement>(null);
+  const starTwoRef = useRef<HTMLElement>(null);
+  const starThreeRef = useRef<HTMLElement>(null);
+  const starFourRef = useRef<HTMLElement>(null);
+  const starFiveRef = useRef<HTMLElement>(null);
+
   useEffect(() => {
     if (!props.interactive) {
       return;
     }
-
-    let starOne: HTMLElement | null = null;
-    let starTwo: HTMLElement | null = null;
-    let starThree: HTMLElement | null = null;
-    let starFour: HTMLElement | null = null;
-    let starFive: HTMLElement | null = null;
 
     let insideStarOne = false;
     let insideStarTwo = false;
@@ -44,93 +44,89 @@ export default function Stars(props: Props) {
     let insideStarFour = false;
     let insideStarFive = false;
 
-    const starsInterval = setInterval(() => {
-      starOne = document.getElementById(`${props.id}-star-one`);
-      starTwo = document.getElementById(`${props.id}-star-two`);
-      starThree = document.getElementById(`${props.id}-star-three`);
-      starFour = document.getElementById(`${props.id}-star-four`);
-      starFive = document.getElementById(`${props.id}-star-five`);
+    if (
+      !starOneRef ||
+      !starTwoRef ||
+      !starThreeRef ||
+      !starFourRef ||
+      !starFiveRef
+    ) {
+      return;
+    }
 
-      if (!starOne || !starTwo || !starThree || !starFour || !starFive) {
-        return;
-      }
-
-      clearInterval(starsInterval);
-
-      window.addEventListener("mousemove", event => {
-        if (insideStarOne) {
-          if (event.offsetX < 20) {
-            setNewRating(Rating.POINT_FIVE);
-          } else {
-            setNewRating(Rating.ONE);
-          }
-        } else if (insideStarTwo) {
-          if (event.offsetX < 20) {
-            setNewRating(Rating.ONE_POINT_FIVE);
-          } else {
-            setNewRating(Rating.TWO);
-          }
-        } else if (insideStarThree) {
-          if (event.offsetX < 20) {
-            setNewRating(Rating.TWO_POINT_FIVE);
-          } else {
-            setNewRating(Rating.THREE);
-          }
-        } else if (insideStarFour) {
-          if (event.offsetX < 20) {
-            setNewRating(Rating.THREE_POINT_FIVE);
-          } else {
-            setNewRating(Rating.FOUR);
-          }
-        } else if (insideStarFive) {
-          if (event.offsetX < 20) {
-            setNewRating(Rating.FOUR_POINT_FIVE);
-          } else {
-            setNewRating(Rating.FIVE);
-          }
+    window.addEventListener("mousemove", event => {
+      if (insideStarOne) {
+        if (event.offsetX < 20) {
+          setNewRating(Rating.POINT_FIVE);
+        } else {
+          setNewRating(Rating.ONE);
         }
-      });
+      } else if (insideStarTwo) {
+        if (event.offsetX < 20) {
+          setNewRating(Rating.ONE_POINT_FIVE);
+        } else {
+          setNewRating(Rating.TWO);
+        }
+      } else if (insideStarThree) {
+        if (event.offsetX < 20) {
+          setNewRating(Rating.TWO_POINT_FIVE);
+        } else {
+          setNewRating(Rating.THREE);
+        }
+      } else if (insideStarFour) {
+        if (event.offsetX < 20) {
+          setNewRating(Rating.THREE_POINT_FIVE);
+        } else {
+          setNewRating(Rating.FOUR);
+        }
+      } else if (insideStarFive) {
+        if (event.offsetX < 20) {
+          setNewRating(Rating.FOUR_POINT_FIVE);
+        } else {
+          setNewRating(Rating.FIVE);
+        }
+      }
+    });
 
-      starOne.addEventListener("mouseover", event => {
-        insideStarOne = true;
-      });
+    starOneRef.current?.addEventListener("mouseover", event => {
+      insideStarOne = true;
+    });
 
-      starOne.addEventListener("mouseout", event => {
-        insideStarOne = false;
-      });
+    starOneRef.current?.addEventListener("mouseout", event => {
+      insideStarOne = false;
+    });
 
-      starTwo.addEventListener("mouseover", event => {
-        insideStarTwo = true;
-      });
+    starTwoRef.current?.addEventListener("mouseover", event => {
+      insideStarTwo = true;
+    });
 
-      starTwo.addEventListener("mouseout", event => {
-        insideStarTwo = false;
-      });
+    starTwoRef.current?.addEventListener("mouseout", event => {
+      insideStarTwo = false;
+    });
 
-      starThree.addEventListener("mouseover", event => {
-        insideStarThree = true;
-      });
+    starThreeRef.current?.addEventListener("mouseover", event => {
+      insideStarThree = true;
+    });
 
-      starThree.addEventListener("mouseout", event => {
-        insideStarThree = false;
-      });
+    starThreeRef.current?.addEventListener("mouseout", event => {
+      insideStarThree = false;
+    });
 
-      starFour.addEventListener("mouseover", event => {
-        insideStarFour = true;
-      });
+    starFourRef.current?.addEventListener("mouseover", event => {
+      insideStarFour = true;
+    });
 
-      starFour.addEventListener("mouseout", event => {
-        insideStarFour = false;
-      });
+    starFourRef.current?.addEventListener("mouseout", event => {
+      insideStarFour = false;
+    });
 
-      starFive.addEventListener("mouseover", event => {
-        insideStarFive = true;
-      });
+    starFiveRef.current?.addEventListener("mouseover", event => {
+      insideStarFive = true;
+    });
 
-      starFive.addEventListener("mouseout", event => {
-        insideStarFive = false;
-      });
-    }, 100); // end interval
+    starFiveRef.current?.addEventListener("mouseout", event => {
+      insideStarFive = false;
+    });
   }, [props.id, props.interactive]);
 
   function makeStars(rating: Rating) {
@@ -139,92 +135,92 @@ export default function Stars(props: Props) {
       case Rating.ZERO:
         return (
           <>
-            <i id={`${props.id}-star-one`}className={`fa-regular fa-star fa-${props.size}`}></i>
-            <i id={`${props.id}-star-two`}className={`fa-regular fa-star fa-${props.size}`}></i>
-            <i id={`${props.id}-star-three`}className={`fa-regular fa-star fa-${props.size}`}></i>
-            <i id={`${props.id}-star-four`}className={`fa-regular fa-star fa-${props.size}`}></i>
-            <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+            <i ref={starOneRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+            <i ref={starTwoRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+            <i ref={starThreeRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+            <i ref={starFourRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+            <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
           </>
         );
       case Rating.POINT_FIVE:
         return <>
-          <i id={`${props.id}-star-one`}className={`${  styles[`hs-${props.size}`]} fa-regular fa-star-half-stroke fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`${ styles[`hs-${props.size}`]} fa-regular fa-star-half-stroke fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
         </>;
       case Rating.ONE:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
         </>;
       case Rating.ONE_POINT_FIVE:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`${  styles[`hs-${props.size}`]} fa-solid fa-star-half-stroke fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`${ styles[`hs-${props.size}`]} fa-solid fa-star-half-stroke fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
         </>;
       case Rating.TWO:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
         </>;
       case Rating.TWO_POINT_FIVE:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`${  styles[`hs-${props.size}`]} fa-regular fa-star-half-stroke fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`${ styles[`hs-${props.size}`]} fa-regular fa-star-half-stroke fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
         </>;
       case Rating.THREE:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-regular fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
         </>;
       case Rating.THREE_POINT_FIVE:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`${  styles[`hs-${props.size}`]} fa-solid fa-star-half-stroke fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`${ styles[`hs-${props.size}`]} fa-solid fa-star-half-stroke fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
         </>;
       case Rating.FOUR:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-regular fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-regular fa-star fa-${props.size}`}></i>
         </>;
       case Rating.FOUR_POINT_FIVE:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`${  styles[`hs-${props.size}`]} fa-solid fa-star-half-stroke fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`${ styles[`hs-${props.size}`]} fa-solid fa-star-half-stroke fa-${props.size}`}></i>
         </>;
       case Rating.FIVE:
         return <>
-          <i id={`${props.id}-star-one`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-two`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-three`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-four`}className={`fa-solid fa-star fa-${props.size}`}></i>
-          <i id={`${props.id}-star-five`}className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starOneRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starTwoRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starThreeRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starFourRef} className={`fa-solid fa-star fa-${props.size}`}></i>
+          <i ref={starFiveRef} className={`fa-solid fa-star fa-${props.size}`}></i>
         </>;
     }
   }
